d1db2962ce6fa355d58d3adf47e2efec4f0a05bf
Terry Blain <terry.blain31@gmail.com>
2015-04-15 19:48:19 +0200

    ajout de tests unitaires pour Adresse

--- test/unit/com/tblabsau/universite/s8/amc/projet/AdresseSpec.groovy
+++ test/unit/com/tblabsau/universite/s8/amc/projet/AdresseSpec.groovy
@@ -2,6 +2,7 @@ package com.tblabsau.universite.s8.amc.projet
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -9,12 +10,44 @@ import spock.lang.Specification
 @TestFor(Adresse)
 class AdresseSpec extends Specification {
 
-    def setup() {
+    @Unroll
+    void "teste la validite d'une adresse valide"(String unNumero, String uneRue, String unCodePostal, String uneVille) {
+
+        given: "une adresse initialisee avec ses parametres non nuls et non vides"
+        Adresse adresse = new Adresse(numero: unNumero, rue: uneRue, codePostal: unCodePostal, ville: uneVille)
+
+        expect: "l'adresse est valide"
+        adresse.validate() == true
+
+        where:
+        unNumero    |   uneRue              |   unCodePostal    |   uneVille
+        "10"        |   "Rue de la Paix"    |   "31120"         |   "Roquettes"
+        null        |   "Rue de la Paix"    |   "31120"         |   "Roquettes"
+
     }
 
-    def cleanup() {
+    @Unroll
+    void "test l'invalidite d'une adresse non valide"(String unNumero, String uneRue, String unCodePostal, String uneVille) {
+
+        given: "une adresse initialisee avec un de ses parametres vide ou null"
+        Adresse adresse = new Adresse(numero: unNumero, rue: uneRue, codePostal: unCodePostal, ville: uneVille)
+
+        expect: "l'adresse est invalide"
+        adresse.validate() == false
+
+        where:
+        unNumero    |   uneRue              |   unCodePostal    |   uneVille
+        ""          |   "Rue de la Paix"    |   "31120"         |   "Roquettes"
+        null        |   "Rue de la Paix"    |   "31120"         |   "Roquettes"
+        "10"        |   ""                  |   "31120"         |   "Roquettes"
+        "10"        |   null                |   "31120"         |   "Roquettes"
+        "10"        |   "Rue de la Paix"    |   ""              |   "Roquettes"
+        "10"        |   "Rue de la Paix"    |   null            |   "Roquettes"
+        "10"        |   "Rue de la Paix"    |   "31120"         |   ""
+        "10"        |   "Rue de la Paix"    |   "31120"         |   null
+
     }
 
-    void "test something"() {
-    }
+
+
 }
