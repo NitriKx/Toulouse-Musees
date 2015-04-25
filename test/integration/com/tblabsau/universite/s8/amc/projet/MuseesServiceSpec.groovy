56b940efaee16b1085eb41f13ae11315359171de
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-04-25 06:05:47 +0200

    Convertion du test unitaire du service "MuseeServiceSpec" en test d'intégration car le plugin CSVReader n'est pas chargé dans les test unitaires des services

---
+++ test/integration/com/tblabsau/universite/s8/amc/projet/MuseesServiceSpec.groovy
@@ -0,0 +1,27 @@
+package com.tblabsau.universite.s8.amc.projet
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(MuseesService)
+class MuseesServiceSpec extends Specification {
+
+    def setup() {
+        service.gestionnaireService = new GestionnaireService()
+    }
+
+    void "test chargement des musées à partir d'un fichier CSV"() {
+
+        given: "un chemin dans le classpath vers un fichier CSV valide contenant des informations sur des musées"
+        String cheminResourceFichierCSVMusee = "initial-data/Musee.csv"
+
+        when: "le service charge la liste des musées du fichier et les enregistre dans la base de données"
+        service.loadFromCSVFile(cheminResourceFichierCSVMusee)
+
+        then: "le service doit avoir 13 musées"
+        service.getMuseeList().size() == 13
+    }
+}
